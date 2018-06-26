1a5c8d49a97c0e79fa9b2a0bda44528a7ce59b4b
silvanet <silvanet@gmail.com>
2018-06-26 18:47:34 -0400

    June 26 update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//I forgot who I got this from, but I'll find it and give him credit. Did a lot of research and tried to pick from what I liked best
+//to learn
+
 $(window).on('resize', function() {
   'use strict';
   var element	= document.querySelector('#banner'),
@@ -9,7 +12,7 @@ $(window).on('resize', function() {
       wScrollBefore	= 0,
       wScrollDiff	= 0; 
       window.addEventListener('scroll', function () {
-        // Hide the displayed menu. If you want to scroll, you're obviously not interested in the options.
+        // Hide the displayed menu. I want to scroll, so not interested in the display menu options.
         $('.navbar-collapse').collapse('hide');
         $('.navbar-toggle').addClass('collapsed').blur();
         if($(window).width() <= 768) {
@@ -20,27 +23,31 @@ $(window).on('resize', function() {
           wScrollDiff = wScrollBefore - wScrollCurrent;
           elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
 
-          // scrolled to the very top; element sticks to the top
+          // scroll to top; element sticks to top
           if(wScrollCurrent <= 0) {
               element.style.top = '0px';
-          } // scrolled up; element slides in
+          } 
+          // scroll up; element slides in
             else if(wScrollDiff > 0) {
               element.style.top = (elTop > 0? 0 : elTop) + 'px'; 
-          } // scrolled down
+          } 
+          // scroll down
             else if(wScrollDiff < 0) {
-              // scrolled to the very bottom; element slides in
+              // scroll to bottom; element slides in
               if(wScrollCurrent + wHeight >= dHeight - elHeight) {  
                 element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
-              } // scrolled down; element slides out
+              } 
+              // scroll down; element slides out
                 else { element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px'; }
             }
           wScrollBefore = wScrollCurrent;
-        } //
+        } 
+        //
           else element.style.top = '0px';
       });
 }).resize();
 
-// Floating label headings for the contact form
+// Floating label headings for contact form
 $(function() {$("body")
  .on("input propertychange", ".form-item",function(e) {
   $(this).toggleClass("form-item-filled",!! $(e.target).val());})
@@ -50,14 +57,14 @@ $(function() {$("body")
   $(this).removeClass("form-item-focused");});
 });
 
-// Highlight the top nav as scrolling occurs
+// Highlight top nav as scroll
 $('body').scrollspy({target: '.navbar-fixed-top'})
 
-// Closes the Responsive Menu on Menu Item Click
+// Close Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
   $(".navbar-collapse").collapse('hide');});
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// jQuery for page scrolling - requires jQuery Easing plugin
 $(function() {$('body').on('click', 'a.scrollable', function(event) {
   var $anchor = $(this);
   $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
